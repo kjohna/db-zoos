5b4c4fa916dd85aafee740d6436c275f433ba19a
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-19 14:15:13 -0700

    configure/test knex

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3'
+  },
+  useNullAsDefault: true
+}
+const db = knex(knexConfig);
 
 const server = express();
 
@@ -7,6 +17,15 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos');
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 
 const port = 3300;
 server.listen(port, function() {
