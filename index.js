85bc46d22b00d1994749fb85f6010f509db67a3b
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-19 14:45:39 -0700

    PUT endpoint working

--- index.js
+++ index.js
@@ -61,6 +61,25 @@ server.post('/api/zoos', async (req, res) => {
   }
 });
 
+// update zoo
+server.put('/api/zoos/:id', async (req, res) => {
+  try {
+    const numUpdated = await db('zoos')
+      .where({ id: req.params.id })
+      .update(req.body);
+    if (numUpdated > 0 ) {
+      const updatedZoo = await db('zoos')
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json(updatedZoo);
+    } else {
+      res.status(404).json({ message: 'No zoo with that id!'});
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
