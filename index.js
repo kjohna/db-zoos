cca8fc4989998445ab9288135f50cffa9ed7a846
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-19 14:23:32 -0700

    GET endpoints working

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+// list all zoos
 server.get('/api/zoos', async (req, res) => {
   try {
     const zoos = await db('zoos');
@@ -26,6 +28,19 @@ server.get('/api/zoos', async (req, res) => {
   }
 });
 
+// list zoo by id
+server.get('/api/zoos/:id', async (req, res) => {
+  try {
+    const zoo = await db('zoos').where({ id: req.params.id });
+    if (zoo.length < 1) {
+      res.status(404).json({ message: 'No zoo with that id!' });
+    } else {
+      res.status(200).json(zoo);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
